ae1237a7ca0cafe57731b451414938e1216af7c3
ksachtleben <72809779+ksachtleben@users.noreply.github.com>
2024-02-16 18:45:33 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dojo_beam_transforms',
+    name='dojo-beam-transforms',
     version='0.1',
     packages=find_packages(),
     install_requires=[
