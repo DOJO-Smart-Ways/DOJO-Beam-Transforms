5c206de7db7cb9e7e5aabb36d194e3f82621cfdb
ksachtleben <72809779+ksachtleben@users.noreply.github.com>
2024-02-16 17:49:35 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dojo_beam_transforms',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+      fsspec==2023.5.0,
+      gcsfs==2023.5.0,
+      openpyxl==3.0.10,
+      pandas==1.5.3,
+      pandas-datareader==0.10.0,
+      pandas-gbq==0.17.9
+    ],
+)
