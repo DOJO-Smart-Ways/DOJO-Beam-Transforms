a8da4d237bdb13bf2c113f142c3eb16290438b5f
ksachtleben <72809779+ksachtleben@users.noreply.github.com>
2024-02-16 17:59:56 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-      fsspec==2023.5.0,
-      gcsfs==2023.5.0,
-      openpyxl==3.0.10,
-      pandas==1.5.3,
-      pandas-datareader==0.10.0,
-      pandas-gbq==0.17.9
+        'fsspec==2023.5.0',
+        'gcsfs==2023.5.0',
+        'openpyxl==3.0.10',
+        'pandas==1.5.3',
+        'pandas-datareader==0.10.0',
+        'pandas-gbq==0.17.9',
     ],
 )
