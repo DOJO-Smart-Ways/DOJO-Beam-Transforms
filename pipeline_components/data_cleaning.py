b228e51f7e64151d366f50fea39247fe2f57e1ff
ksachtleben <72809779+ksachtleben@users.noreply.github.com>
2024-04-08 18:54:40 -0300

    fix(): identação da classe TrimValues

--- pipeline_components/data_cleaning.py
+++ pipeline_components/data_cleaning.py
@@ -448,24 +448,24 @@ class ExtractDecimalFromString(beam.DoFn):
     """
 
 
-    class TrimValues(beam.DoFn):
-        def __init__(self, columns):
-            """
-            Initializes the TrimValues instance.
+class TrimValues(beam.DoFn):
+    def __init__(self, columns):
+        """
+        Initializes the TrimValues instance.
 
-            Args:
-                columns (list of str): A list of column names to trim spaces from.
-            """
-            self.columns = columns
+        Args:
+            columns (list of str): A list of column names to trim spaces from.
+        """
+        self.columns = columns
 
-        def process(self, element):
-            """
-            Processes each element, trimming leading and trailing spaces from specified columns.
+    def process(self, element):
+        """
+        Processes each element, trimming leading and trailing spaces from specified columns.
 
-            Args:
-                element (dict): The input element to process, where keys are column names.
-            """
-            for column in self.columns:
-                if column in element and isinstance(element[column], str):
-                    element[column] = element[column].strip()
-            yield element
+        Args:
+            element (dict): The input element to process, where keys are column names.
+        """
+        for column in self.columns:
+            if column in element and isinstance(element[column], str):
+                element[column] = element[column].strip()
+        yield element
