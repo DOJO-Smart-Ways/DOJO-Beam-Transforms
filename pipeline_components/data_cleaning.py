8b0e93c39133a6cb827b7cf35e4d274c1f1f93ee
Gustavo Silva <gustavo.b.silva@electrolux.com>
2024-04-08 15:54:59 -0300

    feat: Add TrimValues transform
    Add a new TrimValues transform to trim leading and trailing spaces from specified columns in a Beam pipeline.
    
    The TrimValues transform allows users to specify a list of column names from which to remove spaces. It processes each element in the input PCollection, trimming spaces from the specified columns if they are of string type.

--- pipeline_components/data_cleaning.py
+++ pipeline_components/data_cleaning.py
@@ -446,3 +446,26 @@ class ExtractDecimalFromString(beam.DoFn):
     - It extracts decimal values from input strings, including cases where negative values are represented within parentheses.
     - If decimal values are already separeted by dots, please try a simple cast to Decimal Type. 
     """
+
+
+    class TrimValues(beam.DoFn):
+        def __init__(self, columns):
+            """
+            Initializes the TrimValues instance.
+
+            Args:
+                columns (list of str): A list of column names to trim spaces from.
+            """
+            self.columns = columns
+
+        def process(self, element):
+            """
+            Processes each element, trimming leading and trailing spaces from specified columns.
+
+            Args:
+                element (dict): The input element to process, where keys are column names.
+            """
+            for column in self.columns:
+                if column in element and isinstance(element[column], str):
+                    element[column] = element[column].strip()
+            yield element
