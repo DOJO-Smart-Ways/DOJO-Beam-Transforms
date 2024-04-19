e9ea1244c660c0f41fdaf76e5dc49424c5f60a6f
Gustavo Silva <gustavo.b.silva@electrolux.com>
2024-04-19 10:13:47 -0300

    feat: Add new class to remove accents from columns

--- pipeline_components/data_cleaning.py
+++ pipeline_components/data_cleaning.py
@@ -469,3 +469,41 @@ class TrimValues(beam.DoFn):
             if column in element and isinstance(element[column], str):
                 element[column] = element[column].strip()
         yield element
+
+
+class RemoveAccents(beam.DoFn):
+    """
+    A class to remove accents from specified columns of elements.
+
+    Attributes:
+        columns (list): A list of column names from which accents will be removed.
+    """
+
+    def __init__(self, columns):
+        """
+        Initializes the RemoveAccents object with the specified columns.
+
+        Args:
+            columns (list): A list of column names from which accents will be removed.
+        """
+        self.columns = columns
+
+    def process(self, element):
+        """
+        Processes each element in the pipeline, removing accents from specified columns.
+
+        Args:
+            element (dict): The element to be processed.
+
+        Returns:
+            list: A list containing the processed element with accents removed.
+        """
+        from unidecode import unidecode
+        try:
+            for col in self.columns:
+                if col in element and element[col] is not None:
+                    element[col] = unidecode(str(element[col]))
+        except Exception as e:
+            print(f"Error while removing accents: {e}")
+        return [element]
+
