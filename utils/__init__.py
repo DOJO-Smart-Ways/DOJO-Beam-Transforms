a89b6a1ea61b595cf303f851ca6fd1af5e8ece85
Gustavo Silva <gustavo.b.silva@electrolux.com>
2024-06-13 15:46:03 -0300

    fix: keep testing new module

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,6 +3,10 @@ from .gcp_utils import *
 
 import inspect, sys
 
-functions_list = [name for name, _ in inspect.getmembers(sys.modules[__name__], inspect.isfunction)]
+def is_public_function(obj):
+    return inspect.isfunction(obj) and not obj.__name__.startswith('_')
+
+# Obtém todas as funções públicas do módulo gcp_utils
+functions_list = [name for name, func in inspect.getmembers(sys.modules[__name__], is_public_function)]
 
 __all__ = functions_list
