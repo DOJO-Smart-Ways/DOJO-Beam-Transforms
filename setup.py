3978c1d89264494ec5272397aaa6bf46850f7eed
Gustavo Silva <gustavo.b.silva@electrolux.com>
2024-08-20 18:00:58 -0300

    feat: Initiate package version 1.0.0 (https://semver.org/)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dojo-beam-transforms',
-    version='0.1',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
         'apache-beam[dataframe,gcp,interactive]==2.54.0',
