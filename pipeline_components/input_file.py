86099bf3cdafcc37845143ed9957d743dc00e3fe
gbsDojo <gustavo.silva@dojo.technology>
2025-04-14 14:57:23 +0000

    fix: Removed duplicated methods in input utils

--- pipeline_components/input_file.py
+++ pipeline_components/input_file.py
@@ -1,6 +1,4 @@
-# input_file.py
 import apache_beam as beam
-
 
 class ProcessCSVFiles(beam.DoFn):
     def __init__(self, delimiter=';'):
@@ -99,52 +97,6 @@ def read_and_apply_headers(pipeline, input_header, input_file_csv, delimiter=';'
     return (body | f'ApplyHeaders {identifier_suffix}' >> beam.ParDo(ApplyHeadersFn(), beam.pvalue.AsSingleton(headers)))
 
 
-class ProcessPDF(beam.DoFn):
-    def process(self, readable_file):
-        import fitz  # PyMuPDF
-        import re
-        row_pattern = re.compile(
-            r'(\d+)\s+(\d+)\s+([\w\s-]+?)\s+([\w\s-]+?)\s+(\d+\.\d+\.\d+)\s+([A-Z\d]+)\s*([\w\s]+?)\s*([D|W])'
-        )
-
-        with readable_file.open() as file_handle:
-            doc = fitz.open(stream=file_handle.read(), filetype="pdf")
-            for page_num in range(len(doc)):
-                page = doc.load_page(page_num)
-                text = page.get_text()
-
-                # Once the table start is identified, extract rows
-                matches = row_pattern.findall(text)
-                for match in matches:
-                    row_data = {
-                        "EMPLOYEE_ID": match[1],
-                        "FIRST_NAME": match[2],
-                        "LAST_NAME": match[3],
-                        "COST_CENTRE": match[5],
-                        "DEPARTMENT": match[6].strip() if match[6] else "",
-                        "ENDING_PAYMENT": '',
-                        "DIMISSAL_DATE": match[4],
-                        "GROUP": match[7]
-                    }
-                    yield row_data
-
-def read_pdf(pipeline, input_file, identifier=''):
-    from apache_beam.io import fileio
-    identifier_suffix = f"_{identifier}" if identifier else ""
-    return (pipeline
-        | f'Match PDF Files {identifier_suffix}' >> fileio.MatchFiles(input_file)
-        | f'Read Matches {identifier_suffix}' >> fileio.ReadMatches()
-        | f'Process PDFs {identifier_suffix}' >> beam.ParDo(ProcessPDF())
-    )
-
-def read_csv(pipeline, input_file, delimiter=';', identifier=''):
-    import pandas as pd
-    identifier_suffix = f"_{identifier}" if identifier else ""
-    return (
-        pipeline
-        | f'Create File Path Excel {identifier_suffix}' >> beam.Create([input_file])
-        | f'Read Excel {identifier_suffix}' >> beam.FlatMap(lambda file: pd.read_csv(file, delimiter=delimiter, engine='python').to_dict('records'))
-    )
 
 
 def read_txt(pipeline, input_file, skip_header_lines=0, identifier=''):
@@ -204,40 +156,4 @@ def read_excel_transpose_and_dict(pipeline, input_file, tab, header_row=1):
         pipeline
         | 'Create File Path' >> beam.Create([input_file])
         | 'Read and Transpose CSV' >> beam.FlatMap(transpose_and_convert_to_dict)
-    )
-
-
-class ProcessExcelFiles(beam.DoFn):
-    def __init__(self, skip_rows=None, tab=None):
-        self.skip_rows = skip_rows
-        self.tab = 0 if tab is None else tab
-
-    def process(self, file):
-        import pandas as pd
-        from apache_beam.io.filesystems import FileSystems
-        # Read Excel file, assuming it's the first sheet; adjust if necessary
-        with FileSystems.open(file.metadata.path) as f:
-            df = pd.read_excel(f, skiprows=self.skip_rows, sheet_name=self.tab, dtype=str)
-
-        # If tab is an integer, fetch the actual sheet name for inclusion in SOURCE
-        if isinstance(self.tab, int):
-            xls = pd.ExcelFile(file.metadata.path)
-            sheet_name = xls.sheet_names[self.tab]
-        else:
-            sheet_name = self.tab
-
-        # Append the file path and sheet name to 'SOURCE'
-        df['SOURCE'] = f"{file.metadata.path}/{sheet_name}"
-
-        # Return records as dictionaries
-        yield from df.to_dict('records')
-
-def read_excels_union(pipeline, input_pattern, identifier='', skiprows=None, tab=None):
-    from apache_beam.io.fileio import MatchFiles, ReadMatches
-    identifier_suffix = f"_{identifier}" if identifier else ""
-    return (
-        pipeline
-        | f'Match Files {identifier_suffix}' >> MatchFiles(input_pattern)
-        | f'Read Matches {identifier_suffix}' >> ReadMatches()
-        | f'Process Excel Files {identifier_suffix}' >> beam.ParDo(ProcessExcelFiles(skip_rows=skiprows, tab=tab))
     )
