7cb2c66037dff3c7a5b6760fc1796f9011470c6f
gbsDojo <gustavo.silva@dojo.technology>
2026-02-11 15:10:08 -0300

    feat: update Dockerfile and setup.py for Python 3.13 and Apache Beam 2.71.0

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='dojo-beam-transforms',
-    version='3.0.0',
+    version='3.1.0',
     packages=find_packages(),
     install_requires=[
-        'apache-beam[dataframe,gcp,interactive]==2.64.0',
+        'apache-beam[dataframe,gcp,interactive]==2.71.0',
         'pandas==2.0.3',
         'numpy==1.26.3',
         'pytz==2025.2',
