6d90712475d88fe2168b93243687a0dd81592695
Lucas-Lorensi <lucas_Lorensi@hotmail.com>
2026-02-19 17:29:40 -0300

    python 11 and 12 support

--- setup.py
+++ setup.py
@@ -5,10 +5,9 @@ setup(
     version='3.0.0',
     packages=find_packages(),
     install_requires=[
-        'apache-beam[dataframe,gcp,interactive]==2.64.0',
-        'pandas==2.0.3',
-        'numpy==1.26.3',
-        'pytz==2025.2',
-        'openpyxl==3.1.5'
+        'apache-beam[dataframe,gcp,interactive]>=2.61.0',  # 2.61+ added Python 3.12 support
+        'pandas>=2.1.0',                                   # 2.1+ has Python 3.12 wheels
+        'pytz>=2021.3',
+        'openpyxl>=3.0.0',
     ],
 )
