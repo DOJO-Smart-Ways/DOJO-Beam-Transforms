2745bf7de826fbfb9c16d637b1e87fb669aa7ebd
Gustavo Bandeira <gustavo.silva@dojo.technology>
2026-04-22 15:24:34 +0000

    feat: update description to pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
+from pathlib import Path
 from setuptools import setup, find_packages
+
+README = Path(__file__).with_name("README.md").read_text(encoding="utf-8")
 
 setup(
     name='dojo-beam-transforms',
     version='3.1.0',
     description='Apache Beam 2.72.0 transforms and utilities for Python 3.12',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/DOJO-Smart-Ways/DOJO-Beam-Transforms',
+    project_urls={
+        'Source': 'https://github.com/DOJO-Smart-Ways/DOJO-Beam-Transforms',
+        'Issues': 'https://github.com/DOJO-Smart-Ways/DOJO-Beam-Transforms/issues',
+    },
     packages=find_packages(),
     install_requires=[
         'apache-beam[dataframe,gcp,interactive]==2.72.0',
